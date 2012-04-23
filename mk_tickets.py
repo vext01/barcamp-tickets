3b1a7d8fe60d634539e37cbc8c6fe4883fdd5a95
vext01 <vext01@gmail.com>
2012-04-23 21:01:03 +0100

    generate PDF

--- mk_tickets.py
+++ mk_tickets.py
@@ -3,6 +3,9 @@
 import sys
 import copy
 import os
+import os.path
+
+OUTDIR = "out"
 
 if len(sys.argv) != 2:
     print("usage: mk_tickets.py <infile>")
@@ -15,7 +18,11 @@ for i in svg_fd:
     svg_buf = svg_buf + i
 svg_fd.close()
 
-# XXX mkdir
+# output directory
+if os.path.exists(OUTDIR):
+    print("output dir '%s' already exists" % (OUTDIR))
+    sys.exit(1)
+os.mkdir(OUTDIR)
 
 # iterate names making tickets
 names_fd = open(sys.argv[1], "r")
@@ -26,12 +33,21 @@ for line in names_fd:
     (fname, lname, role, company) = line.split(",")
     copy_buf = copy.copy(svg_buf)
 
+    outfile_name = "%s/%s-%s_%s" % (OUTDIR, role, lname, fname)
+    print(fname + " " + lname)
+
     # subsitutions
     copy_buf = copy_buf.replace("!!FNAME!!", fname)
     copy_buf = copy_buf.replace("!!LNAME!!", lname)
     copy_buf = copy_buf.replace("!!ROLE!!", role)
     copy_buf = copy_buf.replace("!!COMPANY!!", company)
+
+    out_fd = open(outfile_name + ".svg", "w")
+    out_fd.write(copy_buf)
+    out_fd.close()
+
+    # convert to pdf
+    os.system("inkscape --export-pdf=%s.pdf %s.svg" % \
+            (outfile_name, outfile_name))
     
 names_fd.close()
-
-print(copy_buf)
