4d3a67efae9da591b146dd8952a02489475f4dd2
vext01 <vext01@gmail.com>
2012-04-27 12:56:24 +0100

    - strip names - title case names and company names - second parameter allows us to print for example only sponsor tickets - one dir per ticket type - fix bug with where 2x8 should be 2x4 (8 in total). simplies pdfnup call.

--- mk_tickets.py
+++ mk_tickets.py
@@ -5,12 +5,25 @@ import copy
 import os
 import os.path
 
-OUTDIR = "out"
-OUTPDF = "all.pdf"
+roles = { "ST" : "Staff",
+          "SP" : "Sponsor",
+          "AT" : "Attendee" }
 
-if len(sys.argv) != 2:
-    print("usage: mk_tickets.py <infile>")
+
+def usage():
+    print("usage: mk_tickets.py <infile> <role>")
+    print("role is ST=staff, SP=sponsor, AT=atendee")
     sys.exit(1)
+
+if len(sys.argv) != 3:
+	usage()
+
+if sys.argv[2] not in roles.keys():
+	usage()
+
+role_filter = sys.argv[2]
+out_pdf = roles[role_filter] + ".pdf"
+out_dir = roles[role_filter]
 
 # suck in whole svg (it will do)
 svg_fd = open("ticket.svg", "r")
@@ -20,14 +33,10 @@ for i in svg_fd:
 svg_fd.close()
 
 # output directory
-if os.path.exists(OUTDIR):
-    print("output dir '%s' already exists" % (OUTDIR))
+if os.path.exists(out_dir):
+    print("output dir '%s' already exists, deleting" % (out_dir))
     sys.exit(1)
-os.mkdir(OUTDIR)
-
-roles = { "ST" : "Staff",
-          "SP" : "Sponsor",
-          "AT" : "Attendee" }
+os.mkdir(out_dir)
 
 # iterate names making tickets
 names_fd = open(sys.argv[1], "r")
@@ -38,10 +47,23 @@ for line in names_fd:
         continue
 
     (fname, lname, role, company) = line.split(",")
+
+    # normalise
+    fname = fname.title().strip()
+    lname = lname.title().strip()
+    company = company.title().strip()
+
+    if role not in roles.keys():
+	    print("unknown role %s" % role)
+	    sys.exit(1)
+
+    if role != role_filter:
+	    continue # not interested in this
+
     role = roles[role]
     copy_buf = copy.copy(svg_buf)
 
-    outfile_name = "%s/%s-%s_%s" % (OUTDIR, role, lname, fname)
+    outfile_name = "%s/%s-%s_%s" % (out_dir, role, lname, fname)
     print(fname + " " + lname)
 
     # subsitutions
@@ -55,14 +77,12 @@ for line in names_fd:
     out_fd.close()
 
     # convert to pdf
-    os.system("inkscape --export-pdf=\"%s-%d.pdf\" %s.svg" % \
+    os.system("inkscape --export-pdf=\"%s-%d.pdf\" \"%s.svg\"" % \
             (outfile_name, count, outfile_name))
     count = count + 1
 names_fd.close()
 
-print("Making final %s/%s.pdf" % (OUTDIR, OUTPDF))
 cmd = (("pdfnup --outfile %s/%s --no-twoside --no-landscape --a4paper " + \
-        "--noautoscale true --offset '0em -12cm' --nup 2x8 --scale 1 --quiet out/*.pdf") % (OUTDIR, OUTPDF))
-print(cmd)
+        "--noautoscale true --nup 2x4 --scale 1 --quiet %s/*.pdf") % (out_dir, out_pdf, out_dir))
 os.system(cmd)
-print("Done")
+print("Tickets outputted to %s" % out_pdf)
